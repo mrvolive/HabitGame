25b276e04c4712dbf907df3473aca87346e97539
mrvolive <olivier.maraval@edu.univ-fcomte.fr>
2025-05-07 11:31:34 +0200

    Initial Commit

---
+++ HabitGame/HabitGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitGameApp.swift
+//  HabitGame
+//
+//  Created by maraval olivier on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
